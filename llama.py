ed2b262131a7fb4aad2d5b46332a57ae9d95e39d
David González Martínez <me@davidgonmar.me>
2023-10-15 17:50:31 +0200

    feat: add support for local Llama models

---
+++ llama.py
@@ -0,0 +1,38 @@
+
+from llama_cpp import Llama
+import replicate
+
+
+class LlamaLocal():
+    """Uses llama_cpp locally to generate responses."""
+    def __init__(self, model_path: str):
+        self.llama = Llama(model_path=model_path)
+
+    def generate_response(self, user_prompt: str):
+        """Generate a response using the local model."""
+        llama_pred = self.llama(user_prompt)
+        text = llama_pred.get('choices')[0].get('text')
+        return text
+    
+
+
+class LlamaReplicate():
+    """Uses replicate (remote) to generate responses."""
+    def __init__(self, replicate_model: str, system_prompt: str = ""):
+        self.replicate_model = replicate_model
+        self.system_prompt = system_prompt
+
+    def generate_response(self, user_prompt):
+        """Generate a response using the replicate model."""
+
+        output = replicate.run(
+            self.replicate_model,
+            input={"prompt": user_prompt, "system_prompt": self.system_prompt})
+            # Response comes chunked
+        response_parts = []
+        for item in output:
+            response_parts.append(item)
+        return "".join(response_parts)
+
+       
+        
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-import asyncio
 import os
 from dotenv import load_dotenv
 import discord
-import replicate
+from llama import LlamaLocal, LlamaReplicate
 
 load_dotenv()
 
-
 class DiscordBot(discord.Client):
-    def __init__(self, intents):
+    def __init__(self, intents, local):
         super().__init__(intents=intents)
-        self.replicate_model = os.environ['REPLICATE_MODEL']
-        self.system_prompt = """You are a helpful, respectful and honest assistant.
+        if local:
+            self.llama = LlamaLocal(model_path=os.path.abspath(os.environ['LOCAL_MODEL_PATH']))
+        else:
+            self.llama = LlamaReplicate(
+                replicate_model=os.environ['REPLICATE_MODEL'],
+                system_prompt="""You are a helpful, respectful and honest assistant.
                                 Always answer as helpfully as possible, while being safe. Your answers should not include any harmful,
                                 unethical, racist, sexist, toxic, dangerous, or illegal content. 
                                 Please ensure that your responses are socially unbiased and positive in nature.
                                 If a question does not make any sense, or is not factually coherent, explain why instead of answering
                                 something not correct. If you don't know the answer to a question, please don't
-                                share false information."""
+                                share false information.""")
+            
 
     async def generate_user_prompt(self, channel, sender, limit=5):
         """Generate a formatted prompt. Only messages from the sender or bot will be included."""
@@ -34,22 +37,6 @@ class DiscordBot(discord.Client):
 
         return "\n".join(reversed(messages))
 
-    async def generate_response(self, user_prompt):
-        """Generate a response using the replicate model."""
-        loop = asyncio.get_event_loop()
-
-        def run_replicate():
-            output = replicate.run(
-                self.replicate_model,
-                input={"prompt": user_prompt, "system_prompt": self.system_prompt})
-            # Response comes chunked
-            response_parts = []
-            for item in output:
-                response_parts.append(item)
-            return " ".join(response_parts)
-
-        # Run the model in a separate thread to avoid blocking code
-        return await loop.run_in_executor(None, run_replicate)
 
     async def on_ready(self):
         """Called when the bot is ready and connected to Discord."""
@@ -64,8 +51,8 @@ class DiscordBot(discord.Client):
             async with message.channel.typing():
                 user_prompt = await self.generate_user_prompt(
                     channel=message.channel, sender=message.author)
-                response = await self.generate_response(user_prompt)
-                await message.channel.send(response)
+                resp = self.llama.generate_response(user_prompt=user_prompt)
+                await message.channel.send(resp)
 
         except Exception as exception:
             print(f"An error occurred: {exception}")
@@ -74,7 +61,12 @@ class DiscordBot(discord.Client):
 def bootstrap():
     intents = discord.Intents.default()
     intents.message_content = True
-    bot = DiscordBot(intents=intents)
+    
+
+    mode = os.environ['MODE'].lower()
+    assert mode in {'local', 'replicate'}, f"Invalid mode: {mode}. Must be 'LOCAL' or 'REPLICATE'."
+
+    bot = DiscordBot(intents=intents, local=mode == 'local')
     bot.run(os.environ['DISCORD_API_TOKEN'])
 
 
