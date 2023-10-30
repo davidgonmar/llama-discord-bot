d30784218454df70186edf78685d272586d6f892
David González Martínez <me@davidgonmar.me>
2023-10-30 23:47:14 +0100

    feat: better prompt generation according to llama paper - wip

--- llama_discord_bot/llama.py
+++ llama_discord_bot/llama.py
@@ -22,6 +22,8 @@ class LlamaBase(ABC):
     USER_INPUT_END = "[/INST]"
     SYSTEM_INPUT_START = "<<SYS>>"
     SYSTEM_INPUT_END = "<</SYS>>"
+    MESSAGES_PAIR_START = "<s>"
+    MESSAGES_PAIR_END = "</s>"
 
     def __init__(self, system_prompt: str = ""):
         self.system_prompt = system_prompt
@@ -30,21 +32,68 @@ class LlamaBase(ABC):
     def generate_response(self, messages: list[Message], suffix: str = "") -> str:
         """Generate a response using the model."""
 
-    def _generate_user_prompt(self, messages: list[Message]) -> str:
-        """Generate a formatted user prompt from a list of messages."""
-
-        # Format user input using "[INST]" and "[/INST]" blocks, while bot's messages remain unchanged
-        user_prompts = [self.USER_INPUT_START + message.content + self.USER_INPUT_END if message.user ==
-                        "user" else message.content for message in messages]
-
-        return "\n".join(user_prompts)
-
     def _generate_prompt(self, messages: list[Message], suffix: str) -> str:
         """From a list of messages, generate a prompt, including both the user and system prompts."""
 
-        # Append the system prompt to the user prompt
-        user_prompt = self._generate_user_prompt(messages=messages)
-        return f"{self.SYSTEM_INPUT_START}\n{self.system_prompt}\n{self.SYSTEM_INPUT_END}\n\n{user_prompt}\n\n{suffix}"
+        # The prompting technique is the one from Meta's paper.
+        # A good explanation can be found at https://huggingface.co/blog/llama2#how-to-prompt-llama-2
+
+        if (len(messages) == 0):
+            raise ValueError("Cannot generate prompt from empty messages list")
+
+        # We need to group messages into groups like:
+        # [[user, user, user....], [bot, bot, bot...], [user, user, user...], ...]
+        # basically group consecutive messages from the same author together
+        # we dont know what the first message is, so we need to find out
+
+        current_role = messages[0].user
+        messages_grouped = [[messages[0]]]
+
+        for message in messages[1:]:
+            if message.user == current_role:
+                messages_grouped[-1].append(message)
+            else:
+                messages_grouped.append([message])
+                current_role = message.user
+
+        # append all messages for each sub-list into a single string,
+        #  so for each message group user= messages[0].user and content = all messages in the group
+        messages = [{"user": group[0].user, "content": "".join(
+            [message.content for message in group])} for group in messages_grouped]
+
+        # now, for each pair, format it like
+        """
+        <s>
+        {{ bot_message }}
+        [INST] <<SYS>>
+        {{ system_prompt }}
+        <</SYS>>
+        {{ user_message }} [/INST] </s>
+        """
+        # if there is no bot or user message, simply write "" System prompt and their delimiters only appended on first pair
+
+        prompt = ""
+        i = 0
+        while i < len(messages):
+            message = messages[i]
+            bot_message = message["content"] if message["user"] == "bot" else ""
+            user_message = message["content"] if message["user"] == "user" else ""
+            system_prompt = self.SYSTEM_INPUT_START + \
+                self.system_prompt + self.SYSTEM_INPUT_END if i == 0 else ""
+            prompt += f"""
+            <s>
+            {bot_message}
+            [INST]
+            {system_prompt}
+            {user_message} [/INST] </s>
+            """
+
+            if bot_message != "":
+                i += 1
+            if user_message != "":
+                i += 1
+        print(prompt)
+        return prompt.strip()
 
 
 class LlamaLocal(LlamaBase):
